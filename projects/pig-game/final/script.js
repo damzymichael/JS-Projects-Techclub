f4aa3afc7a64b6218403b13727e61080ad37cc11
damzymichael <105460672+damzymichael@users.noreply.github.com>
2023-09-18 12:49:32 +0100

    pig-game-starter

--- projects/pig-game/final/script.js
+++ projects/pig-game/final/script.js
@@ -1,1 +1,22 @@
 'use strict';
+const scores = [0, 0];
+
+//Select elements
+const score1El = document.getElementById('score--0');
+const score2El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const rollDiceButton = document.querySelector('.btn--roll');
+// console.log(rollDiceButton);
+
+//Starting conditions
+score1El.textContent = 0;
+score2El.textContent = 0;
+diceEl.classList.add('hidden');
+
+rollDiceButton.addEventListener('click', function () {
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+  // console.log(diceNumber);
+  diceEl.src = `dice-${diceNumber}.png`;
+  diceEl.classList.remove('hidden');
+});
+
